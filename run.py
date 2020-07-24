b4159b9e0e6d2fc82a0b5b6499dc12ff7fc18f18
xiaoao-xyz <ao.xiao@xyzrobotics.ai>
2020-07-24 11:01:48 +0800

    保留index.html中的super()

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__, template_folder="template")
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
@@ -15,6 +16,15 @@ def user(name):
     # return '<h1>Welcome, %s</h1>' % name
     return render_template('user.html', name = name)
 
+@app.route('/ua')
+def get_ua():
+    user_agent = request.headers.get('User-Agent')
+    return '<h1>Your User Agent:</h1>\n<strong>%s</strong>' % user_agent
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 
